abb057d78b166cfa2595fba73e3aca729b93043b
J Delaney <j.delaney.sd@gmail.com>
2014-05-08 19:16:29 -0700

    Updated users model

--- server/models/users.js
+++ server/models/users.js
@@ -10,6 +10,34 @@ exports.getAll = function (callback) {
 	database.query('SELECT * FROM user', function (err, rows, fields) {
 		if (err) {
 			callback(err, null);
+			return;
+		}
+
+		callback(null, rows);
+	});
+};
+
+exports.get = function (userId, callback) {
+	database.query('SELECT * FROM user WHERE user_id = ?', [userId], function (err, rows, fields) {
+		if (err) {
+			callback(err, null);
+			return;
+		}
+
+		if (rows === []) {
+			callback(null, null);
+			return;
+		}
+
+		callback(null, rows[0]);
+	});
+};
+
+exports.search = function (params, callback) {
+	var y = database.query('SELECT * FROM user WHERE ?', params, function (err, rows, fields) {
+		if (err) {
+			callback(err, null);
+			return;
 		}
 
 		callback(null, rows);
