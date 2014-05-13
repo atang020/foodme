0ed23d2388c5f9e43526cc39949a6ebba9f1068a
J Delaney <j.delaney.sd@gmail.com>
2014-05-13 16:28:34 -0700

    database model test

---
+++ server/tests/database.js
@@ -0,0 +1,24 @@
+process.env.NODE_ENV = 'test';
+
+var assert = require('assert');
+var database = require('../models/database');
+var userModel = require('../models/users');
+
+describe('Database helper model', function () {
+	it('should truncate the database', function (done) {
+		this.timeout(0);
+		//Add something to the DB to ensure there is data to erase
+		userModel.add({password: '1234', email: 'fj@gj.com', phone: '283-483-3923'}, function (err) {
+			assert.ifError(err);
+
+			database.truncate('do not use in production', function (err) {
+				assert.ifError(err);
+				userModel.getAll(function (err, result) {
+					assert.ifError(err);
+					assert.equal(result.length, 0);
+					done();
+				});
+			});
+		});
+	});
+});
