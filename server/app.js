f3ea6ff5faf38a33d87e8cb9c52857291ca50c9a
Phillip Hodgson <phillipghodgson@gmail.com>
2014-05-15 13:41:06 -0700

    Changed app.js to make orders.js accessible

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ var config = require('./config')();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var orders = require('./routes/orders');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/orders', orders);
 
 /// catch 404 and forwarding to error handler
 app.use(function (req, res, next) {
