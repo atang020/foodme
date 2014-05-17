85bd4bafd0221f2e10875ea45c5a3971904f10f7
Dennis Cao <denniscao94@gmail.com>
2014-05-17 15:45:34 -0700

    tttttt

--- tablet/foodme/src/org/segfault/foodme/MainMenuActivity.java
+++ tablet/foodme/src/org/segfault/foodme/MainMenuActivity.java
@@ -6,16 +6,18 @@ package org.segfault.foodme;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainMenuActivity extends Activity implements ActionBar.TabListener{
