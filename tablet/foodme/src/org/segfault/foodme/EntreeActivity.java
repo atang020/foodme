6e68f5ea892eaa41f1b6b47878206cf87c16df81
unknown <Henry Truong>
2014-05-17 16:08:37 -0700

    testing pages

--- tablet/foodme/src/org/segfault/foodme/EntreeActivity.java
+++ tablet/foodme/src/org/segfault/foodme/EntreeActivity.java
@@ -35,7 +35,7 @@ public class EntreeActivity extends Activity implements ActionBar.TabListener{
 		super.onCreate(savedInstanceState);
 	
 		// Get the view from activity_main.xml
-		setContentView(R.layout.activity_fullscreen);
+		setContentView(R.layout.activity_entree);
 		
 		View decorView = getWindow().getDecorView();
 		
@@ -109,6 +109,7 @@ public class EntreeActivity extends Activity implements ActionBar.TabListener{
 		case "Dessert":
 		case "My Orders":
 		case "Call Waiter":callWaiterPress();
+							break;
 		}
 		
 	}
