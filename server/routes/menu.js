c2a107352a2b6fc9e61f9545811e18d1787fb3dd
Phillip Hodgson <phillipghodgson@gmail.com>
2014-05-18 01:20:56 -0700

    edit menu is pretty cool now. If you're reading this maybe you should check it out?

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-	res.render('menu', {user : {name: 'Phillip'}});
+	var myMenu = {'drinks':{}, 'appetizers':{}, 'entrees':{}, 'desserts':{}};
+	
+	//NOTE! subcategory names can't have spaces (at least for now)
+	myMenu.drinks.subcategories = [{name: 'soda'}, {name: 'beer'}, {name: 'double-beer'}];
+	myMenu.appetizers.subcategories = [{name: 'soups'}, {name: 'salads'}];
+	myMenu.entrees.subcategories = [{name: 'pasta'}, {name: 'chicken'}, {name: 'curries'}, ];
+	myMenu.desserts.subcategories = [{name: 'unhealthy-desserts'}, {name: 'even-more-unhealthy-desserts'}];
+	res.render('menu', {user : {name: 'Phillip'}, categories: myMenu});
 });
 
 module.exports = router;
