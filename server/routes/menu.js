7b55c563b24cfa05c1ce93fc291cc15943c31323
Phillip Hodgson <phillipghodgson@gmail.com>
2014-05-18 20:33:04 -0700

    items now display as passed in

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -5,10 +5,15 @@ router.get('/', function(req, res){
 	var myMenu = {'drinks':{}, 'appetizers':{}, 'entrees':{}, 'desserts':{}};
 	
 	//NOTE! subcategory names can't have spaces (at least for now)
-	myMenu.drinks.subcategories = [{name: 'soda'}, {name: 'beer'}, {name: 'double-beer'}];
-	myMenu.appetizers.subcategories = [{name: 'soups'}, {name: 'salads'}];
-	myMenu.entrees.subcategories = [{name: 'pasta'}, {name: 'chicken'}, {name: 'curries'}, ];
-	myMenu.desserts.subcategories = [{name: 'unhealthy-desserts'}, {name: 'even-more-unhealthy-desserts'}];
+	myMenu.drinks.subcategories = [{name: 'soda', items: []}, {name: 'beer', items: []}, {name: 'double-beer', items: []}];
+	myMenu.appetizers.subcategories = [{name: 'soups', items: []}, {name: 'salads', items: []}];
+	myMenu.entrees.subcategories = [{name: 'pasta', items: []}, {name: 'chicken', items: []}, {name: 'curries', items: []}, ];
+	myMenu.desserts.subcategories = [{name: 'unhealthy-desserts', items: []}, {name: 'even-more-unhealthy-desserts', items: []}];
+	
+	myMenu.drinks.subcategories[0].items = [{name: 'small soda', price: '$1.49'}, 
+											{name: 'medium soda', price: '$2.19'},
+											{name: 'large soda', price: '$2.49'}];
+	
 	res.render('menu', {user : {name: 'Phillip'}, categories: myMenu});
 });
 
