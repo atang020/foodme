19c7a39f85a0b3968343b81deb36eca775bdaf47
Phillip Hodgson <phillipghodgson@gmail.com>
2014-05-20 12:16:56 -0700

    more hacked-together javascript to show more descriptive deletion messages

--- server/public/js/menu.js
+++ server/public/js/menu.js
@@ -1,4 +1,17 @@
+//adds an item
 function addItem(cat, subcat) {
     var row = $('#' + subcat).find('tbody').find('.hidden');
 	row.after('<tr>' + row.html() + '</tr>');
+}
+
+//prepares deletion of item
+function setConfirmModal(item, cat, subcat) {
+	var dialog = $('#confirmModal').find('.confirm-description');
+	dialog.text('are you sure you want to delete item ' + item + '?');
+}
+
+//prepares deletion of subcategory
+function setConfirmModal(cat, subcat) {
+	var dialog = $('#confirmModal').find('.confirm-description');
+	dialog.text('are you sure you want to delete subcategory ' + subcat + '?');
 }
