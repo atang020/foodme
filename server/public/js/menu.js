5588f2fe4140fe0ed5e5d288be8f3a949bfb65fd
Phillip Hodgson <phillipghodgson@gmail.com>
2014-05-20 12:39:53 -0700

    Improved the confirm deletion javascript a lil bit

--- server/public/js/menu.js
+++ server/public/js/menu.js
@@ -1,17 +1,31 @@
 //adds an item
-function addItem(cat, subcat) {
+function addItem(subcat, cat) {
     var row = $('#' + subcat).find('tbody').find('.hidden');
 	row.after('<tr>' + row.html() + '</tr>');
 }
 
 //prepares deletion of item
-function setConfirmModal(item, cat, subcat) {
+function setConfirmModalItem(item, subcat, cat) {
+	var dialog = $('#confirmModal').find('.confirm-description');
+	dialog.text('are you sure you want to delete item ' + item + '?');
+	$('#confirmModal').modal();
+}
+
+//prepares deletion of subcategory
+function setConfirmModalSubcat(subcat, cat) {
+	var dialog = $('#confirmModal').find('.confirm-description');
+	dialog.text('are you sure you want to delete subcategory ' + subcat + '?');
+	$('#confirmModal').modal();
+}
+
+//prepares rename of item
+function setInputModal(item, subcat, cat) {
 	var dialog = $('#confirmModal').find('.confirm-description');
 	dialog.text('are you sure you want to delete item ' + item + '?');
 }
 
-//prepares deletion of subcategory
-function setConfirmModal(cat, subcat) {
+//prepares rename of subcategory
+function setInputModal(subcat, cat) {
 	var dialog = $('#confirmModal').find('.confirm-description');
 	dialog.text('are you sure you want to delete subcategory ' + subcat + '?');
 }
