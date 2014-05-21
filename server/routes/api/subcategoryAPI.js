be0bdc0f457a0cecc47223e659c9125b498bc8f3
J Delaney <j.delaney.sd@gmail.com>
2014-05-21 07:15:48 -0700

    API for the models

---
+++ server/routes/api/subcategoryAPI.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var subcategoryModel = require('../../models/subcategoryModel');
+
+var router = express.Router();
+
+router.get('/', function(req, res){
+	subcategoryModel.getAll(function (err, orders) {
+		if (err) {
+			res.send(500);
+		}
+		res.json(orders);
+	});
+});
+
+router.get('/:subcategoryId', function (req, res) {
+	subcategoryModel.get(req.params.subcategoryId, function (err, ticket) {
+		if (err) {
+			res.send(500);
+		}
+		res.json(ticket);
+	})
+});
+
+router.post('/', function (req, res) {
+	subcategoryModel.add(req.body, function (err, id) {
+		if (err) {
+			res.send(500);
+		}
+
+		res.send(id.toString());
+	});
+});
+
+router.put('/', function (req, res) {
+	subcategoryModel.update(req.body, function (err) {
+		if (err) {
+			res.send(500);
+		} else {
+			res.send(200);
+		}
+	})
+});
+
+module.exports = router;
