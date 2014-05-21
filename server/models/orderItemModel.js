741bd9213ddde5d46b479df429e8eadaf24bbd6d
Ryan <ryanmcclure4@gmail.com>
2014-05-20 19:15:27 -0700

    changed orderItemModel.js: order to ticket

--- server/models/orderItemModel.js
+++ server/models/orderItemModel.js
@@ -1,16 +1,16 @@
 var database = require('./database');
 
-function verify(orderItem) {
-	// order_id, menu_item_id, and quantity must be NOT NULL
-	if (orderItem.quantity === undefined || orderItem.quantity === null) {
+function verify(ticketItem) {
+	// ticket_id, menu_item_id, and quantity must be NOT NULL
+	if (ticketItem.quantity === undefined || ticketItem.quantity === null) {
 		return new Error('A quantity must be provided.');
 	}
 
-	if (orderItem.order_id === undefined || orderItem.order_id === null) {
-		return new Error('Order ID not specified.');
+	if (ticketItem.ticket_id === undefined || ticketItem.ticket_id === null) {
+		return new Error('ticket ID not specified.');
 	}
 
-	if (orderItem.menu_item_id === undefined || orderItem.menu_item_id === null) {
+	if (ticketItem.menu_item_id === undefined || ticketItem.menu_item_id === null) {
 		return new Error('Menu Item ID not specified.');
 	}
 
@@ -18,12 +18,12 @@ function verify(orderItem) {
 }
 
 /**
- * Returns data for all order items. The callback gets two arguments (err, data).
+ * Returns data for all ticket items. The callback gets two arguments (err, data).
  *
  * @param callback
  */
 exports.getAll = function (callback) {
-	database.query('SELECT * FROM order_item', null, function (err, rows) {
+	database.query('SELECT * FROM ticket_item', null, function (err, rows) {
 		if (err) {
 			callback(err, null);
 			return;
@@ -33,13 +33,13 @@ exports.getAll = function (callback) {
 };
 
 /**
- * Gets the order_item with the specified id
- * @param orderId
+ * Gets the ticket_item with the specified id
+ * @param ticketId
  * @param callback
  *
  */
-exports.get = function (orderId, callback) {
-	database.query('SELECT * FROM order_item WHERE order_item_id = ?', [orderId], function (err, rows) {
+exports.get = function (ticketId, callback) {
+	database.query('SELECT * FROM ticket_item WHERE ticket_item_id = ?', [ticketId], function (err, rows) {
 		if (err) {
 			callback(err, null);
 			return;
@@ -55,13 +55,13 @@ exports.get = function (orderId, callback) {
 };
 
 /**
- * Gets the order_items that match the given parameters
+ * Gets the ticket_items that match the given parameters
  * @param params
  * @param callback
  *
  */
 exports.search = function (params, callback) {
-	database.query('SELECT * FROM order_item WHERE ?', params, function (err, rows) {
+	database.query('SELECT * FROM ticket_item WHERE ?', params, function (err, rows) {
 		if (err) {
 			callback(err, null);
 			return;
@@ -71,13 +71,13 @@ exports.search = function (params, callback) {
 };
 
 /**
- * Inserts a new order_item. The callback gets two arguments (err, data).
+ * Inserts a new ticket_item. The callback gets two arguments (err, data).
  *
- * @param orderItem the data to be inserted into the order_item table
+ * @param ticketItem the data to be inserted into the ticket_item table
  * @param callback
  */
-exports.add = function (orderItem, callback) {
-	var err = verify(orderItem);
+exports.add = function (ticketItem, callback) {
+	var err = verify(ticketItem);
 	if (err) {
 		callback(err);
 		return;
@@ -86,11 +86,11 @@ exports.add = function (orderItem, callback) {
 
 	// if kitchenStatus is undefined, set to 0
 	//TODO: WE NEED TO DECIDE WHAT THE DIFFERENT KITCHEN STATUS VALUES MEAN
-	orderItem.kitchen_status = orderItem.kitchen_status === undefined ? 0 : orderItem.kitchen_status;
-	orderItem.notes = orderItem.notes === undefined ? '' : orderItem.notes;
+	ticketItem.kitchen_status = ticketItem.kitchen_status === undefined ? 0 : ticketItem.kitchen_status;
+	ticketItem.notes = ticketItem.notes === undefined ? '' : ticketItem.notes;
 
-	database.query('INSERT INTO order_item (order_id, menu_item_id, quantity, notes, kitchen_status)' +
-			'VALUES (?, ?, ?, ?, ?)', [orderItem.order_id, orderItem.menu_item_id, orderItem.quantity, orderItem.notes, orderItem.kitchen_status],
+	database.query('INSERT INTO ticket_item (ticket_id, menu_item_id, quantity, notes, kitchen_status)' +
+			'VALUES (?, ?, ?, ?, ?)', [ticketItem.ticket_id, ticketItem.menu_item_id, ticketItem.quantity, ticketItem.notes, ticketItem.kitchen_status],
 		function (err, result) {
 			if (err) {
 				callback(err);
@@ -101,15 +101,15 @@ exports.add = function (orderItem, callback) {
 		});
 };
 
-exports.update = function (orderItem, callback) {
-	var err = verify(orderItem);
+exports.update = function (ticketItem, callback) {
+	var err = verify(ticketItem);
 	if (err) {
 		callback(err);
 		return;
 	}
 
-	database.query('UPDATE order_item order_id = ?, menu_item_id = ?, quantity = ?, notes = ?, kitchen_status = ? WHERE order_item_id = ?',
-		[orderItem.order_id, orderItem.menu_item_id, orderItem.quantity, orderItem.notes, orderItem.kitchen_status, orderItem.order_item_id], function (err) {
+	database.query('UPDATE ticket_item ticket_id = ?, menu_item_id = ?, quantity = ?, notes = ?, kitchen_status = ? WHERE ticket_item_id = ?',
+		[ticketItem.ticket_id, ticketItem.menu_item_id, ticketItem.quantity, ticketItem.notes, ticketItem.kitchen_status, ticketItem.ticket_item_id], function (err) {
 
 			if (err) {
 				callback(err);
@@ -120,18 +120,18 @@ exports.update = function (orderItem, callback) {
 };
 
 /**
- * Deletes an order_item. The callback gets two arguments (err, data).
+ * Deletes an ticket_item. The callback gets two arguments (err, data).
  *
- * @param orderItem the data to be deleted from the order_item table
+ * @param ticketItem the data to be deleted from the ticket_item table
  * @param callback
  */
-exports.remove = function (orderItem, callback) {
-    if (orderItem.order_item_id === null) {
-        callback(new Error('Invalid order item: no id present'));
+exports.remove = function (ticketItem, callback) {
+    if (ticketItem.ticket_item_id === null) {
+        callback(new Error('Invalid ticket item: no id present'));
         return;
     }
 
-    database.query('DELETE FROM order_item WHERE order_item_id = ?', orderItem.order_item_id, function (err) {
+    database.query('DELETE FROM ticket_item WHERE ticket_item_id = ?', ticketItem.ticket_item_id, function (err) {
         if (err) {
             callback(err);
             return;
