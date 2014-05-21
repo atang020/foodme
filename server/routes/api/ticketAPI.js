e21592a65c16019353345805d32519a7f1ab6246
J Delaney <j.delaney.sd@gmail.com>
2014-05-20 20:20:05 -0700

    ticket api

---
+++ server/routes/api/ticketAPI.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var ticketModel = require('../../models/ticketModel');
+
+var router = express.Router();
+
+router.get('/', function(req, res){
+	ticketModel.getAll(function (err, orders) {
+		if (err) {
+			res.send(500);
+		}
+		res.json(orders);
+	});
+});
+
+router.get('/:ticketId', function (req, res) {
+	ticketModel.get(req.params.ticketId, function (err, ticket) {
+		if (err) {
+			res.send(500);
+		}
+		res.json(ticket);
+	})
+});
+
+router.post('/', function (req, res) {
+	ticketModel.add(req.body, function (err, id) {
+		if (err) {
+			res.send(500);
+		}
+
+		res.send(id.toString());
+	});
+});
+
+router.put('/', function (req, res) {
+	ticketModel.update(req.body, function (err) {
+		if (err) {
+			res.send(500);
+		} else {
+			res.send(200);
+		}
+	})
+});
+
+module.exports = router;
