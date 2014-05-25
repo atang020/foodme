ba41a9462e17888376672b4b970ba62dad329f2f
Payam Shahidi <rockinguitarguyx@yahoo.com>
2014-05-24 20:21:48 -0700

    Will be done with this soon. Git has not been working for me lately.

---
+++ server/test/settingModel.test.js
@@ -0,0 +1,9 @@
+process.env.NODE_ENV = 'test';
+
+var assert = require('assert');
+var database = require('../models/database');
+var userModel = require('../models/settingModel');
+
+describe('Setting helper model', function() {
+
+}
