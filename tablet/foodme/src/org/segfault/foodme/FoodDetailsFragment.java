bcbb88c56bc9c6e36b84650fa022e46827f1b0d0
Henry Truong <hetruong@ucsd.edu>
2014-05-26 17:58:26 -0700

    added button for submit customer rating

--- tablet/foodme/src/org/segfault/foodme/FoodDetailsFragment.java
+++ tablet/foodme/src/org/segfault/foodme/FoodDetailsFragment.java
@@ -7,6 +7,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -21,6 +22,7 @@ public class FoodDetailsFragment extends Fragment{
 	TextView foodDescription;
 	TextView leaveARating;
 	ImageView foodImage;
+	Button submitButton;
 	
 	int lastPosition = -1;
     @Override
@@ -33,23 +35,27 @@ public class FoodDetailsFragment extends Fragment{
     @Override
     public void onStart() {
         super.onStart();
-
-        updateFoodDetails(lastPosition);
-    }
-    public void updateFoodDetails(int position)
-    {
+        
     	foodDescription = (TextView) getActivity().findViewById(R.id.food_description);
 		foodImage = (ImageView)getActivity().findViewById(R.id.food_image);
 		ratingNumber = (TextView) getActivity().findViewById(R.id.review_number);
     	ratingBar = (RatingBar) getActivity().findViewById(R.id.ratingBar);
     	customerRating = (RatingBar) getActivity().findViewById(R.id.customerRating);
     	leaveARating = (TextView) getActivity().findViewById(R.id.leave_review);
+    	submitButton = (Button) getActivity().findViewById(R.id.submit_button);
     	ratingBar.setOnTouchListener(new OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
                 return true;
             }
         });
     	ratingBar.setFocusable(false);
+    	
+        updateFoodDetails(lastPosition);
+    }
+    
+    public void updateFoodDetails(int position)
+    {
+
     	float exa = 3;
     	ratingBar.setRating(exa);
     	ratingNumber.setText("Rating Number : " + exa);
@@ -60,6 +66,7 @@ public class FoodDetailsFragment extends Fragment{
     		foodImage.setVisibility(View.GONE);			
     		customerRating.setVisibility(View.GONE);
     		leaveARating.setVisibility(View.GONE);
+    		submitButton.setVisibility(View.GONE);
 			getActivity().findViewById(R.id.review_number).setVisibility(View.GONE);
 			getActivity().findViewById(R.id.ratingBar).setVisibility(View.GONE);
 		}
@@ -69,6 +76,8 @@ public class FoodDetailsFragment extends Fragment{
     		foodImage.setVisibility(View.VISIBLE);
     		customerRating.setVisibility(View.VISIBLE);
     		leaveARating.setVisibility(View.VISIBLE);
+    		submitButton.setVisibility(View.VISIBLE);
+    		customerRating.setRating(0);
 			getActivity().findViewById(R.id.review_number).setVisibility(View.VISIBLE);
 			getActivity().findViewById(R.id.ratingBar).setVisibility(View.VISIBLE);
 			switch(position)
