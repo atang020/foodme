b018b65bb052c9b3d89b2f0f1b71d244f557319f
Henry Truong <hetruong@ucsd.edu>
2014-05-27 19:02:21 -0700

    fixed some formating

--- tablet/foodme/src/org/segfault/foodme/FoodDetailsFragment.java
+++ tablet/foodme/src/org/segfault/foodme/FoodDetailsFragment.java
@@ -80,7 +80,7 @@ public class FoodDetailsFragment extends Fragment{
 
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
-						Toast ratingConfirm = Toast.makeText(getActivity().getApplicationContext(),"Thank you for your rating",Toast.LENGTH_SHORT);
+						Toast ratingConfirm = Toast.makeText(getActivity().getApplicationContext(),"Thank you for rating this item",Toast.LENGTH_SHORT);
 						ratingConfirm.show();
 						customerRating.setRating(0);
 					}
