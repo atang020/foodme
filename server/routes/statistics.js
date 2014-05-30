7b380656a8f1789dde5e11f669614fc50958c786
Phillip Hodgson <phillipghodgson@gmail.com>
2014-05-30 14:11:58 -0700

    statistics page connected to database

--- server/routes/statistics.js
+++ server/routes/statistics.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
 routeHelper = require('../routes/routeHelper');
+var menuItemModel = require('../models/menuItemModel');
 
 router.get('/', function (req, res) {
 	routeHelper.redirectIfLoggedOut(req, res, function(loggedIn) {
 		if(loggedIn){
-			res.render('statistics', { user: {email: req.cookies.email}});
+			menuItemModel.getAll(function (err, menu_items) {
+				if (err) {
+					res.send(500, 'error connecting to database');
+				}
+				res.render('statistics', {user: {email: req.cookies.email}, items: menu_items});
+			});
 		}
 	});
 });
