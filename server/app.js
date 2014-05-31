51bf020fbd506024698c5d8c6610338153b904f7
J Delaney <j.delaney.sd@gmail.com>
2014-05-31 16:27:36 -0700

    server side uploads

--- server/app.js
+++ server/app.js
@@ -30,35 +30,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(multer({
-	dest: './public/uploads/',
-	onFileUploadStart: function (file) {
-		console.log('multer: onFileUploadStart', file);
-	},
-	onFileUploadData: function (file, data) {
-		console.log('multer: onFileUploadData', file, data);
-	},
-	onFileUploadComplete: function (file) {
-		console.log('multer: onFileUploadComplete', file);
-	},
-	onParseStart: function () {
-		console.log('multer: onParseStart');
-	},
-	onParseEnd: function () {
-		console.log('multer: onParseEnd');
-	},
-	onError: function () {
-		console.log('multer: onError');
-	},
-	onFilesLimit: function () {
-		console.log('multer: onFilesLimit');
-	},
-	onFieldsLimit: function () {
-		console.log('multer: onFieldsLimit');
-	},
-	onPartsLimit: function () {
-		console.log('multer: onPartsLimit');
-	}
-}));
+	dest: '/root/foodme/server/public/uploads/'}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
