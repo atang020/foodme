06c4c2fe1ffabc45268a7513785a576f787ddc3d
Phillip Hodgson <phillipghodgson@gmail.com>
2014-05-30 21:32:58 -0700

    added the files I forgot to add to my commit...this should make uploading actually work on the actual server now

---
+++ server/public/js/orders.js
@@ -0,0 +1,62 @@
+var confirmCallback;
+
+//page loaded
+$(document).ready(function () {
+
+	//$('[id^=itemProtoitem]').hide();
+	
+});
+
+
+//prepares deletion of an order
+function setConfirmModal(id) {
+	$.get('/api/ticketItems/' + id, function (data) {
+		showConfirmModal('are you sure you want to delete order #' + data.ticket_item_id + '?');
+	});
+	confirmCallback = function () {
+		$.ajax({
+			url: '/api/ticketitems/' + id,
+			type: 'DELETE',
+			success: function (response) {
+				var item = $('#order' + id);
+				item.fadeOut(300, function () { item.remove(); });
+				closeConfirmModal();
+			},
+			error: function (xhr, ajaxOptions, thrownError) {
+				alert('something went wrong: ' + thrownError);
+			}
+		});
+	}
+}
+
+//marks an order as been delivered (kitchen_status of 10)
+function markDelivered(id) {
+	$.get('/api/ticketitems/' + id, function (data) {
+		data.kitchen_status = 10;
+		$.ajax({
+			url: '/api/ticketitems/' + id,
+			type: 'PUT',
+			data: data,
+			success: function (response) {
+				var item = $('#order' + id);
+				item.fadeOut(300, function () { item.remove(); });
+				closeConfirmModal();
+			},
+			error: function (xhr, ajaxOptions, thrownError) {
+				alert('something went wrong: ' + thrownError);
+			}
+		});
+	});
+}
+
+//closes the confirm modal
+function closeConfirmModal() {
+	var textbox = $('#confirmModal').modal('hide');
+}
+
+//shows the confirm modal with specified text
+function showConfirmModal(text) {
+	var dialog = $('#confirmModal').find('.confirm-description');
+	dialog.text(text);
+	$('#confirmModal').modal();
+}
