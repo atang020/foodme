bf65ed158ea141784f04e6fb2f6deb10254aeb08
Ryan <ryanmcclure4@gmail.com>
2014-05-31 14:55:43 -0700

    asdf

--- tablet/foodme/src/org/segfault/foodme/SplashScreenActivity.java
+++ tablet/foodme/src/org/segfault/foodme/SplashScreenActivity.java
@@ -72,6 +72,7 @@ public class SplashScreenActivity extends Activity
 	    
 	    // Insert new Ticket into server table and retrieve ticketId
 	    ticket = new Ticket ();
-	    new CreateTicket().execute();    
+	    new CreateTicket().execute();
+	    new CallWaiter().execute();
 	}
 }
