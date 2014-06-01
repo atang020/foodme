26ebf1e4f374f8f46bb95f6d450b861e072a32c0
J Delaney <j.delaney.sd@gmail.com>
2014-06-01 14:09:54 -0700

    removed deleted field for tickets

--- server_structure.sql
+++ server_structure.sql
@@ -28,8 +28,7 @@ CREATE TABLE `ticket` (
   `table_number` INT NOT NULL,
   `ticket_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `checked_out` TINYINT NOT NULL DEFAULT 0,
-  `call_waiter_status` TINYINT NOT NULL DEFAULT 0,
-  `deleted` TINYINT NOT NULL DEFAULT 0,
+  `call_waiter_status` TINYINT NOT NULL DEFAULT 0
   PRIMARY KEY (`ticket_id`))
 ENGINE = InnoDB;
 
