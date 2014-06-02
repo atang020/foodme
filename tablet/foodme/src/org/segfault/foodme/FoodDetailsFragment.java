c087e854b34d448aabd32b507c2a9c15e5e93288
Henry Truong <hetruong@ucsd.edu>
2014-06-01 17:14:17 -0700

    customer reviews

--- tablet/foodme/src/org/segfault/foodme/FoodDetailsFragment.java
+++ tablet/foodme/src/org/segfault/foodme/FoodDetailsFragment.java
@@ -2,8 +2,10 @@ package org.segfault.foodme;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -15,10 +17,12 @@ import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -109,12 +113,21 @@ public class FoodDetailsFragment extends Fragment{
 								
 								@Override
 								public void onClick(DialogInterface dialog, int which) {
-									Toast ratingConfirm = Toast.makeText(getActivity().getApplicationContext(),"Thank you for rating this item",Toast.LENGTH_SHORT);
-									ratingConfirm.show();
-									Review customerReview =  new Review(0, foodDetails.getMenuItemId(lastMenuItemIndex), name.getText().toString(), 
-											(short)customerRating.getRating(), review.getText().toString(), "");
-									new SendReview().execute(customerReview);
-									customerRating.setRating(0);
+									if(name.getText().toString().length() > 0 && review.getText().toString().length() > 0)
+									{
+										Toast ratingConfirm = Toast.makeText(getActivity().getApplicationContext(),"Thank you for rating this item",Toast.LENGTH_SHORT);
+										ratingConfirm.show();
+										Review customerReview =  new Review(0, foodDetails.getMenuItemId(lastMenuItemIndex), name.getText().toString(), 
+												(short)customerRating.getRating(), review.getText().toString(), "");
+										new SendReview().execute(customerReview);
+										customerRating.setRating(0);
+									}
+									else
+									{
+										Toast empty = Toast.makeText(getActivity().getApplicationContext(),"Name and/or review empty",Toast.LENGTH_SHORT);
+										empty.show();
+										customerRating.setRating(0);
+									}
 
 								}
 							});
@@ -294,7 +307,6 @@ public class FoodDetailsFragment extends Fragment{
 			public void onClick(View arg0) {
 				AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
 				
-				dialogBuilder.setTitle("Customer Ratings");
 				
 				dialogBuilder.setPositiveButton("Done", new DialogInterface.OnClickListener()
 				{
@@ -305,8 +317,45 @@ public class FoodDetailsFragment extends Fragment{
 					}
 					
 				});
+
+				
+				ArrayList<Review> reviews = foodReview.getReviewByMenuItemId(foodDetails.getMenuItemId(lastMenuItemIndex));
+				
+				ArrayList<String>customerRatings = new ArrayList<String>();
+				String customerName;
+				String customerReview;
+				if(reviews.size() > 0)
+				{
+					for(int i = 0; i < reviews.size(); i++)
+					{
+						customerName = reviews.get(i).getReviewer();
+						customerReview = reviews.get(i).getReviewText();
+						if(!customerName.equals(""))
+						{
+							customerRatings.add(reviews.get(i).getReviewer() + ":\n" + reviews.get(i).getReviewText());
+						}
+					}
+				}
+				else
+				{
+					customerRatings.add("No customer reviews available");
+				}
+				ArrayAdapter<String> hi = new ArrayAdapter<String>(getActivity(),
+		                android.R.layout.simple_list_item_1,
+		                customerRatings);
+				dialogBuilder.setTitle("Customer Ratings");
+				dialogBuilder.setAdapter(hi, new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface arg0, int arg1) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+				});
 				AlertDialog dialog = dialogBuilder.create();
 				dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+
 				dialog.show();
 			}
     		
@@ -362,5 +411,6 @@ public class FoodDetailsFragment extends Fragment{
 			price.setText("Price: $" +  itemPrice.toString());
 		}
 		lastMenuItemIndex = menuItemIndex;
+		System.out.println(lastMenuItemIndex + "asdf");
     }
 }
