d5bfd6ae3d57289e2bb0af2a491f26a4609a86e3
Payam Shahidi <rockinguitarguyx@yahoo.com>
2014-06-02 18:50:19 -0700

    WOOOOOOOO You can now pick and choose items to send to kitchen. Also modified some Dialog Prompts and Toasts.

--- tablet/foodme/src/org/segfault/foodme/MyOrderActivity.java
+++ tablet/foodme/src/org/segfault/foodme/MyOrderActivity.java
@@ -35,23 +35,22 @@ public class MyOrderActivity extends Activity implements ActionBar.TabListener, 
 	private DrawerLayout subcategoryLayout;
 	private ListView subcategoryList;
 	private AlertDialog.Builder dialogBuilder;
-
 	private ViewPager mViewPager;
 	ArrayList<TicketItem> item = SplashScreenActivity.orders;
 	ArrayList<TicketItem> sentItems = new ArrayList<TicketItem>();
 	ArrayAdapter<String> adapter; 
 	private AlertDialog.Builder dialogBuild;
 	private AlertDialog.Builder dialogBuild1;
-	//private AppSectionsPagerAdapter mAppSectionsPagerAdapter
 	Button button;
 	Button restButton;
+	short trueCheck = 1;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	
+		
 		// Get the view from activity_main.xml
 		setContentView(R.layout.activity_my_order);
-		
 		View decorView = getWindow().getDecorView();
 		
 	    int mUIFlag = 
@@ -60,10 +59,6 @@ public class MyOrderActivity extends Activity implements ActionBar.TabListener, 
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
 	   decorView.setSystemUiVisibility(mUIFlag);
-	   
-       
-      // mAppSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager());
-
        // Set up the action bar.
        final ActionBar actionBar = getActionBar();
 
@@ -71,7 +66,7 @@ public class MyOrderActivity extends Activity implements ActionBar.TabListener, 
        // parent.
        actionBar.setHomeButtonEnabled(false);
 
-       // Specify that we will be displaying tabs in the action bar.
+       // Display tabs in the action bar.
        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
        actionBar.addTab(actionBar.newTab().setText("Home").setTabListener(this),false);
        actionBar.addTab(actionBar.newTab().setText("Drinks").setTabListener(this),false);
@@ -84,8 +79,6 @@ public class MyOrderActivity extends Activity implements ActionBar.TabListener, 
        mViewPager = (ViewPager) findViewById(R.id.pager);
 
        // Set the adapter for the list view
-       // subcategoryList.setAdapter(new ArrayAdapter<String>(this,
-       //        R.layout.test_layout, subcategoryNames));
        button = (Button) findViewById(R.id.button1);
        list = (ListView) findViewById(R.id.listView1);	
        subtotal = (TextView) findViewById(R.id.textView6);
@@ -93,6 +86,7 @@ public class MyOrderActivity extends Activity implements ActionBar.TabListener, 
        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice);
        subtotalVal = subTotal();
   		
+        // Display items that were added to order
   		for(int i = 0; i<item.size(); i++) {
   			adapter.add(item.get(i).toString());
   		}
@@ -100,52 +94,121 @@ public class MyOrderActivity extends Activity implements ActionBar.TabListener, 
   		list.setAdapter(adapter);
   		subtotal.setText(subtotalVal);
   		
+  		// "Send To Kitchen"
   		button.setOnClickListener(new View.OnClickListener(){
   			public void onClick(View v){
   				confirmation();
   			}
   		});
   		
+  		// "Final Checkout"
   		restButton.setOnClickListener(new View.OnClickListener() {
   			public void onClick(View v) {
   				final_checkout();
   			}
   		});
   		
+  		// Array that determines if an item is checked or not
+  		//checkArray = new int[item.size()];
+  		
+  		// Methods for items in the list
   		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {  
+  			   
   			   public void onItemSelected(AdapterView parentView, View childView, 
   			                                                         int position, long id) 
   			   {  
-  				   //none
+  				   // TODO
   			   }
 
   			   public void onNothingSelected(AdapterView parentView) {  
-  				   //none
+  				   // Do nothing
   			   }
 
+  			// Clicking on an item prompts an edit option to display
   			@Override
   			public void onItemClick(AdapterView<?> parent, View view,
   					int position, long id) {
-  				// TODO Auto-generated method stub
   				editOrder(position); 
   				adapter.notifyDataSetChanged();
   				}  
-  			});         
+  			}); 
+  		
+  		list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {  
+					// Clicking on an item prompts an edit option to display
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				toggleStatus(position);
+				adapter.notifyDataSetChanged();
+				return true;
+				}  
+			}); 	
 	}
 	
-/*	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-	    // Inflate the menu items for use in the action bar
-	    MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.main_action_bar, menu);
-	    return super.onCreateOptionsMenu(menu);
-	}*/
+	private void toggleStatus(final int position)
+	{
+		dialogBuilder = new AlertDialog.Builder(this);
+		
+		dialogBuilder.setTitle("Change Send Status");
+		dialogBuilder.setMessage("Would you like to change status of item?");
+		dialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				String s1 = item.get(position).getCheckStatus();
+				short tempStatus = 2;
+				
+				if(s1 == "Ready to send!")
+					tempStatus = 1;
+				
+				else if(s1 == "Not ready to send.") 
+					tempStatus = 0;
+				 				
+				TicketItem temp = new TicketItem(item.get(position).getOrderId(), item.get(position).getMenuItemId(), 
+						item.get(position).getMenuItemIndex(), item.get(position).getQuantity(), item.get(position).getNotes(),
+						item.get(position).getKitchenStatus(), item.get(position).getMenuItemName(), item.get(position).getPrice());
+				
+				if(tempStatus == 1)
+				{
+					temp.setCheckStatusNotReady();
+				}
+				
+				else if (tempStatus == 0)
+				{
+					temp.setCheckStatusReady();
+				}
+					
+				
+				adapter.remove(item.get(position).toString());
+				item.remove(position);
+				item.add(temp);
+				adapter.add(item.get((item.size()-1)).toString());
+				
+				Toast toggleConfirm= Toast.makeText(getApplicationContext(),"Item status has been changed.",Toast.LENGTH_SHORT);
+				toggleConfirm.show();
+			}
+		});
+		
+		dialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				Toast waiterConfirm = Toast.makeText(getApplicationContext(),"Item status has NOT been changed..",Toast.LENGTH_SHORT);
+				new CallWaiter().execute();
+				waiterConfirm.show();					
+			}
+		});
+		
+		AlertDialog dialog = dialogBuilder.create();
+		dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+		dialog.show();
+	}
+	
+	// "Final Checkout"
 	private void final_checkout() {
 		dialogBuild1 = new AlertDialog.Builder(this);
-     	//LinearLayout layout = new LinearLayout(getApplicationContext());
 		dialogBuild1.setTitle("Check Please");
 		dialogBuild1.setMessage("Are you sure you want your check?");
-	  	//dialogBuild1.setView(layout);
 		dialogBuild1.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int which)
 			{
@@ -158,11 +221,12 @@ public class MyOrderActivity extends Activity implements ActionBar.TabListener, 
 					new SendTicketItems().execute(item.get(i));
 				}
 				
-				Toast makeText = Toast.makeText(getApplicationContext(),"Successfully checked out. A waiter will be with you shortly", Toast.LENGTH_SHORT);
+				Toast makeText = Toast.makeText(getApplicationContext(),"Successfully checked out. A waiter will be with you shortly.", Toast.LENGTH_SHORT);
 				makeText.show();
 				item = null;
 				adapter = null;
 				
+				// Switch to Splash Screen
 				Intent homeIntent = new Intent(MyOrderActivity.this,
 						SplashScreenActivity.class);
 				homeIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
@@ -172,93 +236,98 @@ public class MyOrderActivity extends Activity implements ActionBar.TabListener, 
 	 });
 	     
 	    	dialogBuild1.setPositiveButton("No", new DialogInterface.OnClickListener() {
-
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-			// TODO Auto-generated method stub
-				Toast makeText = Toast.makeText(getApplicationContext(),"Check will not be sent, yet", Toast.LENGTH_SHORT);
+				Toast makeText = Toast.makeText(getApplicationContext(),"Check will not be sent, yet.", Toast.LENGTH_SHORT);
 				makeText.show();
 			}
 		});
+	    	
 			AlertDialog dialogConfirm = dialogBuild1.create();
 			dialogConfirm.show();
 	}
+	
+	 // "Send To Kitchen"
 	 private void confirmation(){
      	dialogBuild1 = new AlertDialog.Builder(this);
-     	//LinearLayout layout = new LinearLayout(getApplicationContext());
 		dialogBuild1.setTitle("Send To Kitchen");
 		dialogBuild1.setMessage("Are you sure?");
-	  	//dialogBuild1.setView(layout);
+		
 		dialogBuild1.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int which)
-			{
+			{		
+				short showMessage = 0;
+				// Set kitchen status for checked items
 				for(int i = 0; i < item.size(); i++) {
-					short a = 1;
+					short a = 1;		
 					item.get(i).setKitchenStatus(a);
 				}
+				
+				// Executed send for checked items
 				for(int i = 0; i < item.size(); i++) {
-					new SendTicketItems().execute(item.get(i));
+						new SendTicketItems().execute(item.get(i));
 				}
-				Toast makeText = Toast.makeText(getApplicationContext(),"Order has been sent to the kitchen", Toast.LENGTH_SHORT);
-				makeText.show();
 				
-				// TODO Grey out or remove items after pressing Send To Kitchen
-				// adapter.remove(item.get(position).toString());
-				/*String s1 = item.get(0).toString2();
-				adapter.remove(item.get(0).toString());
-				adapter.add(s1);*/
-				System.out.println("item size: " + item.size());
 				int itemSize = item.size();
+		
+				// Variable keeps track of orders that have been sent
+				int incr = 0;
+				
+				// Clear sent items from ListView
 				for(int i = 0; i < itemSize; i++)
-				{	
-					System.out.println("item name: " + item.get(0).getMenuItemName());
-					System.out.println("ITEM SIZEX: " + item.size());
-				  //sentItems.add(item.get(i));
-				  //sentString = sentItems.get(i).toString2();
-				  //adapter.add(sentString);
-				  adapter.remove(item.get(0).toString());
-				  item.remove(0);
+				{	 
+					String readyString = "Ready to send!";
+					String inString = item.get(incr).getCheckStatus();
+					
+					if(readyString.compareTo(inString) == 0)
+					{
+						adapter.remove(item.get(incr).toString());					  
+						adapter.add(item.get(incr).toString2());
+						item.remove(incr);
+						showMessage = 1;
+					}
+					
+					else
+						incr++;
 				}
 				
-				//adapter.remove(item.get(position).toString());
-				//item.remove(position);
+				if(showMessage == 1) {
+					Toast makeText = Toast.makeText(getApplicationContext(),"Order has been sent to the kitchen.", Toast.LENGTH_SHORT);
+					makeText.show();
+				}
 			}
 	 });
 	     
 	    	dialogBuild1.setPositiveButton("No", new DialogInterface.OnClickListener() {
-
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-			// TODO Auto-generated method stub
-				Toast makeText = Toast.makeText(getApplicationContext(),"Order has not been sent to kitchen.", Toast.LENGTH_SHORT);
+				Toast makeText = Toast.makeText(getApplicationContext(),"Order has NOT been sent to kitchen.", Toast.LENGTH_SHORT);
 				makeText.show();
-
 			}
 		});
+	    	
 			AlertDialog dialogConfirm = dialogBuild1.create();
 			dialogConfirm.show();
 	  }
-
 
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,
 			long id) {
 		// TODO Auto-generated method stub
-		
 	}
 	 
+	// This is prompted when user clicks on an item in the ListView
 	private void editOrder(final int position) {
-		dialogBuild = new AlertDialog.Builder(this);
-		
+		dialogBuild = new AlertDialog.Builder(this);		
 		final EditText input = new EditText(this);
 		input.setHint("Notes");
 		final EditText quantity = new EditText(this);
 		quantity.setHint("Quantity");
-		//dialogBuild.setView(input);
 		LinearLayout layout = new LinearLayout(getApplicationContext());
 		layout.setOrientation(LinearLayout.VERTICAL);
 		layout.addView(input);
 		layout.addView(quantity);
+		dialogBuild.setTitle("Edit Item");
 		dialogBuild.setView(layout);
 		dialogBuild.setNegativeButton("Done", new DialogInterface.OnClickListener() {
 		
@@ -271,13 +340,25 @@ public class MyOrderActivity extends Activity implements ActionBar.TabListener, 
 			
 			try {
 				quantityNum = Short.parseShort(quantityVal);
-			}catch(NumberFormatException e) {
-				Toast makeText1 = Toast.makeText(getApplicationContext(),"Give me a valid integer", Toast.LENGTH_SHORT);
+			}
+			
+			catch(NumberFormatException e) {
+				Toast makeText1 = Toast.makeText(getApplicationContext(),"Please enter a valid integer.", Toast.LENGTH_SHORT);
 				makeText1.show();
 				quantityNum = item.get(position).getQuantity();
 			}
 			
-			if(value.trim().length() > 0)
+			if(value.startsWith("-")) {
+				Toast negInput = Toast.makeText(getApplicationContext(),"Please enter a positive integer.", Toast.LENGTH_SHORT);
+				negInput.show();
+			}
+			
+			else if(value.trim().length() <= 0) {
+				Toast realInput = Toast.makeText(getApplicationContext(),"Please enter something.", Toast.LENGTH_SHORT);
+				realInput.show();
+			}
+			
+			else
 				notesVal = value;
 			
 			TicketItem temp = new TicketItem(item.get(position).getOrderId(), item.get(position).getMenuItemId(), 
@@ -300,10 +381,12 @@ public class MyOrderActivity extends Activity implements ActionBar.TabListener, 
 			{
 				adapter.remove(item.get(position).toString());
 				item.remove(position);
-				Toast makeText = Toast.makeText(getApplicationContext(),"Deleted at Index: " + position, Toast.LENGTH_SHORT);
+				//Toast makeText = Toast.makeText(getApplicationContext(),"Deleted at Index: " + position, Toast.LENGTH_SHORT);
+				Toast makeText = Toast.makeText(getApplicationContext(),"Item deleted." + position, Toast.LENGTH_SHORT);
 				makeText.show();
 			}
 		});
+		
 		
 		
 		dialogBuild.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
@@ -383,14 +466,6 @@ public class MyOrderActivity extends Activity implements ActionBar.TabListener, 
 		case "Call Waiter":
 			callWaiterPress();
 			break;
-		
-		/* TODO
-		case "My Order":
-			Intent orderIntent = new Intent(MyOrderActivity.this,
-					MyOrderActivity.class);
-			orderIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-			startActivity(orderIntent);
-			break;*/
 			
 		default:
 			break;
